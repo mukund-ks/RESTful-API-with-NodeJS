ac090a9d92dc8b70263cabbf1237500f2f921e49
Mukund Kumar <banananarick69@gmail.com>
2023-06-07 22:02:47 +0530

    JWT signing

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.post("/signup", (req, res, next) => {
     User.find({ email: req.body.email })
@@ -21,7 +22,7 @@ router.post("/signup", (req, res, next) => {
                             password: hash
                         });
                         user.save()
-                            .then(result => {
+                            .then(() => {
                                 res.status(201).json({ message: "User Created" });
                             })
                             .catch(err => {
@@ -31,6 +32,38 @@ router.post("/signup", (req, res, next) => {
                     }
                 });
             }
+        });
+});
+
+router.post("/login", (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({ message: "Authorization failed" });
+            } else {
+                bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                    if (err) {
+                        return res.status(401).json({ message: "Authorization failed" });
+                    }
+                    if (result) {
+                        const token = jwt.sign(
+                            { email: user[0].email, userID: user[0]._id }, 
+                            process.env.JWT_KEY,
+                            { expiresIn: "1h" }
+                        );
+                        return res.status(200).json({
+                            message: "Authorization successfull",
+                            token: token,
+                        });
+                    }
+                    res.status(401).json({ message: "Authorization failed" });
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
         });
 });
 
